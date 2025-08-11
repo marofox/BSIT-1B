2984b9a31b25ef1f573fec89b488e4d347c9ce89
Mermer <mjlaspinas060107@gmail.com>
2025-08-11 17:15:05 +0800

    Initial commit

---
+++ activity4.py
@@ -0,0 +1,4 @@
+name = input("Type your name here -->")
+
+print("your name has ",
+len(name),"characters")
---
+++ activity5.py
@@ -0,0 +1,4 @@
+name = eval(input("Type any character here ..."))
+
+print("The data type of name is ",
+type(name))
