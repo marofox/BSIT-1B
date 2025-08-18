ba116a88dbe58f27083f54895a8b5faa86162509
Mermer <mjlaspinas060107@gmail.com>
2025-08-18 15:42:02 +0800

    activity9.py

---
+++ activity9.py
@@ -0,0 +1,11 @@
+a = 50
+b = 30
+
+c = 40
+d = 50
+username = 'mar'
+password = 'gemini'
+print((username == 'mar') and (password == 'gemini'))
+print( (a > b) and (c < d) )
+print( (a > b) or (c < d) )
+print(not((a > b) or (c < d)))
