7171785e4abf964d0a3d53205b8e88c3135a0279
Mermer <mjlaspinas060107@gmail.com>
2025-08-18 14:49:12 +0800

    Code_Challenge

---
+++ Code_challenge.py
@@ -0,0 +1,2 @@
+name = input("\t\t\tType your name here -->")
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\t\t",name,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
