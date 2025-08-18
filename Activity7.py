fa6340a2897539a2cf3060789482290cf7389f5f
Mermer <mjlaspinas060107@gmail.com>
2025-08-18 15:14:41 +0800

    Activity7.py

---
+++ Activity7.py
@@ -0,0 +1,20 @@
+a = 5
+
+print("The value of a is",a)
+
+a += 10
+a -= 2
+a *= 2
+a /= 25
+
+print("The value of a is",a)
+
+print("The value of a is",a)
+
+a += 60
+a -= 75
+a *= 90
+a /= 100
+
+print("The value of a is",a)
+
