cb1db9c75d814227b70ec0cdf09eace4a8efc3b0
Mermer <mjlaspinas060107@gmail.com>
2025-08-27 15:03:01 +0800

    activity10.py

---
+++ activity10.py
@@ -0,0 +1,12 @@
+username = "Mar"
+password = "gemini"
+
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+
+if uname == username and pword == password:
+	print("ACCESS GRANTED")
+else:
+	print("ACCESS DENIED")
