39caa2aa6bc0ae5d491bc33d678c39fb88637bb0
marofox <mjlaspinas060107@gmail.com>
2025-09-29 13:10:00 +0800

    Create Code_challenge7.py

---
+++ Code_challenge7.py
@@ -0,0 +1,10 @@
+
+#Countdown Timer
+
+down = eval(input('Enter the starting number for countdown:'))
+
+print('\nCountdown Started')
+for x in range(down, 0, -1):
+    print(x)
+    
+print('liftoff!')    
