f3d3febca65bbe8d445acc4cfaa5d9e2b19aa97d
marofox <mjlaspinas060107@gmail.com>
2025-11-24 15:57:32 +0800

    Update activity24_1.py

--- activity24_1.py
+++ activity24_1.py
@@ -1,4 +1,13 @@
-def Mrchair(name):
-    Print("yow{name},kamusta ka?" )
-def Mrchairname(name,loc,age):
-    print("mar","cotta","18")
+def MrTable(name):
+    print(f'yow {name},kamusta ka?' )
+
+def Mrgwapo(name,loc,age):
+    print(f'yow{name} from {loc} - {age} yr\'s old')
+
+    
+def factorial(number):
+    fact = 1
+    for m in range(number, 0, -1):
+        fact *= m
+
+    print(f'The factorial of {number} is {fact}')
